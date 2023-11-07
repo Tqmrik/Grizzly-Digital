ea1d5e195c619db0943d72a523e631cf771d10bb
Temk <steamxxlss@mail.ru>
2023-11-07 22:47:33 +0300

    RegEx solution

--- index.php
+++ index.php
@@ -4,20 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
     <br>
 
-    <form class="container" action = "index.php" method ="post">
+    <form class="container" action="index.php" method="post">
         <div class="mb-3">
-          <label for="phoneNumberInput" class="form-label ">Номер телефона</label>
-          <input type="text" class="form-control" id="phoneNumberInput" name="phoneNumber">
+            <label for="phoneNumberInput" class="form-label ">Номер телефона</label>
+            <input type="tel" class="form-control" id="phoneNumberInput" name="phoneNumber">
+           
         </div>
         <input type="submit" class="btn btn-primary" value="Send"></button>
-      </form>
+    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
@@ -28,15 +30,24 @@
 
 </body>
 
-</html>
 
 <?php
-    // require_once 'Array.php';
-    // $phoneNumber = $_POST["phoneNumber"];
-    $numbers = [
-        array( "mask"=> "+247-####", "cc"=> "AC", "name_en"=> "Ascension", "desc_en"=> "", "name_ru"=> "Остров Вознесения", "desc_ru"=> "" ),
-        array( "mask"=> "+376-###-###", "cc"=> "AD", "name_en"=> "Andorra", "desc_en"=> "", "name_ru"=> "Андорра", "desc_ru"=> "" )];
-    echo $numbers[0]["mask"];
-    $phone = $numbers[1];
+            $numbers = require 'Array_copy2.php';
+            // require_once 'Array.php';
+            if (array_key_exists('phoneNumber', $_POST)) {
+                
+                $phoneNumber = $_POST["phoneNumber"];
+                $foundSomething = false;
+                foreach ($numbers as $arr) {
+                    if (preg_match("/" . $arr["mask"] . "/i", $phoneNumber)) {
+                        echo $arr["name_ru"];
+                        $foundSomething = true;
+                    }
+                }
+                if (!$foundSomething) {
+                    echo "No matches found";
+                }
+            }
+            ?>
 
-?>  
+</html>

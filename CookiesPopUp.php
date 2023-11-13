89693dd2b231543f31283e475caa5dff9aba42e1
Temk <steamxxlss@mail.ru>
2023-11-13 14:32:41 +0300

    End result

--- CookiesPopUp.php
+++ CookiesPopUp.php
@@ -1,64 +1,26 @@
-<!-- for more: https://mdbootstrap.com/docs/standard/extended/cookie-consent/; https://www.google.com/search?q=cookies-popup+bootstrap&newwindow=1&sca_esv=581345929&sxsrf=AM9HkKk-8vy1W7i8JtrLwvUjriaEZ0fUgQ%3A1699657091070&ei=g7VOZbHqA5n_ptQPnY-yoAo&ved=0ahUKEwjxt5yJxLqCAxWZv4kEHZ2HDKQQ4dUDCBA&uact=5&oq=cookies-popup+bootstrap&gs_lp=Egxnd3Mtd2l6LXNlcnAiF2Nvb2tpZXMtcG9wdXAgYm9vdHN0cmFwMgYQABgWGB4yBhAAGBYYHjIIEAAYCBgeGA1IwxZQpwFYqRVwAXgBkAEAmAGJA6AB2hGqAQcwLjEuNC4zuAEDyAEA-AEBwgIKEAAYRxjWBBiwA8ICBBAAGB7CAgYQABgeGArCAggQABgWGB4YD-IDBBgAIEGIBgGQBgg&sclient=gws-wiz-serp -->
-<!-- Button trigger modal -->
-
-<!-- 
-    * ! Basically add a cookie with a live of 1 day and check if it's exists and then execute the script to show that
- -->
-
-    <!-- <div class="fixed-bottom" style="height: 100px; background-color: #17202A;">
-        <div class="container">
-            
-        </div>
-    </div> -->
-
-<!-- 
-<?php   
-
-     setcookie("popupTimeInterval","1",time() + (86400), "/"); 
-
-?> -->
-
-<!-- FILEPATH: /c:/Users/TEMK/Desktop/phpTC/Grizzly-Digital/CookiesPopUp.php -->
-
-<!-- Cookie consent popup -->
-
-<div class="fixed-bottom d-flex align-items-center" style="height: 75px; background-color: #17202A;" <?php echo $acceptButton;?>>
+<div class="fixed-bottom align-items-center" style="height: 75px; background-color: #17202A;">
     <div class="container">
-        <div class="row d-flex align-items-center " >
+        <div class="row d-flex align-items-center ">
             <div class="col-10 " style="color: white;">
-                <p>We use cookies to ensure you get the best experience on our website. By continuing to browse, you consent to our use of cookies. <a href="#">Learn more</a></p>
+                <p>We use cookies to ensure you get the best experience on our website. By continuing to browse, you
+                    consent to our use of cookies. <a href="#">Learn more</a></p>
             </div>
-            <div class="col-2 d-flex justify-content-center">
-                <form action="CookiesPopUp.php" >
-                    <input type="button" class="btn btn-primary" name="acceptButton" value='Accept' method="post"></input>
+            <div class="col-2 d-flex justify-content-center my-2">
+                <form action="index.php" method="post">
+                    <input type="submit" class="btn btn-primary" name="acceptButton" value='Accept'></input>
                 </form>
-                
             </div>
         </div>
     </div>
 </div>
 
+<?php
 
-<?php 
-    $acceptButton = "";
-    if(array_key_exists('acceptButton', $_POST)){
-        echo "Hello";
+    if (array_key_exists('acceptButton', $_POST)) {
+        $acceptBtn = "style='display:none;'";
     }
+
+    $acceptBtn = "";
+    setcookie("popupTimeInterval", "1", time() + (86400), "/");
+
 ?>
-
-
-
-<!-- <script>
-    function acceptCookies() {
-        // Set cookie to expire in 1 year
-        document.cookie = "cookieConsent=true; expires=" + new Date(new Date().getTime() + 31536000000).toUTCString() + "; path=/";
-        // Hide the popup
-        document.querySelector('.fixed-bottom').style.display = 'none';
-    }
-    
-    // Check if cookie exists
-    if (document.cookie.indexOf('cookieConsent=true') > -1) {
-        // Hide the popup
-        document.querySelector('.fixed-bottom').style.display = 'none';
-    }
-</script> -->
--- index.php
+++ index.php
@@ -22,6 +22,52 @@
         <input type="submit" class="btn btn-primary" value="Проверить"></input>
     </form>
 
+
+    <div class="container border shadow-sm my-5 p-5 bg-transparent rounded">
+        <div class="row p-3">
+            <div class="col-7">
+                <div class="row h2 mb-5">Lorem ipsum</div>
+                <div class="row mb-5">
+                    <div class="col-5 px-0">
+                        <div class="h5">Kostzy</div>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-5 px-0">
+                        <div class="h5">Black Friday?</div>
+                        <div>Nam interdum erat non risus vulputate.</div>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-5 px-0">
+                        <div class="h5">Maecenas euismod</div>
+                        <div>Praesent ac eleifend dui, a eleifend justo.</div>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-5 px-0">
+                        <div class="h5">Sed felis sem</div>
+                        <div>Duis rhoncus nibh at metus ultricies tempor.</div>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-5 px-0">
+                        <div class="h5">Sed sollicitudin</div>
+                        <div>Fusce quam ligula, sagittis eget rutrum et.</div>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-5 px-0">
+                        <div class="h5"> Sed in vulputate</div>
+                        <div>Morbi ultrices quam nec ipsum tincidunt.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 d-flex justify-content-center align-items-center">
+                <img src="https://picsum.photos/300/300" alt="" class="rounded w-50 h-50">
+            </div>
+        </div>
+
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
@@ -30,14 +76,14 @@
         crossorigin="anonymous"></script>
 
 </body>
+
 </html>
 
 
 <?php
+
+if (!array_key_exists('popupTimeInterval', $_COOKIE)) {
     require_once "CookiesPopUp.php";
-    
-    if(!array_key_exists('popupTimeInterval', $_COOKIE)){
-        require_once "CookiesPopUp.php";
-    }
+}
 
 ?>
